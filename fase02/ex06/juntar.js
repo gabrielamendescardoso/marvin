066b419f6912a27e83cdf192f9dec20a5fd91895
gabrielamendescardoso <63648564+gabrielamendescardoso@users.noreply.github.com>
2020-04-14 13:23:30 -0300

    chato

--- fase02/ex06/juntar.js
+++ fase02/ex06/juntar.js
@@ -1,7 +1,6 @@
-function juntar (num1, num2){
-    var string1 = num1.toString();
-    var string2 = num2.toString();
-    return string1 + string2
+function juntar(num1, num2){
+    var string1 = num1.toString()
+    var string2 = num2.toString()
+    return parseInt (string1 + string2)
 }
-
 

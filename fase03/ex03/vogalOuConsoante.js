904e8958d5b100102a10404fb1b7c6d67f2adb5a
gabrielamendescardoso <63648564+gabrielamendescardoso@users.noreply.github.com>
2020-05-31 17:07:38 -0300

    razei demaisssssss

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,10 @@
+function vogalOuConsoante (letra){
+    letra = letra.toLowerCase();
+
+     if(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u')
+      return("Vogal");
+  else     
+      return("Consoante");
+}
+
+
